80c932856879f19f5bdaccd060e1ca7dee9679e7
Phil <pbal@fordham.edu>
2024-05-22 12:29:19 -0700

    Babel integration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,71 @@
+const path = require("path");
+
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    // Add Babel loader to handle TypeScript files
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
+            plugins: ["@babel/plugin-syntax-top-level-await"],
+          },
+        },
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: /node_modules/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
+            plugins: ["@babel/plugin-syntax-top-level-await"],
+          },
+        },
+      ],
+    });
+
+    // Ensure WebAssembly files are handled correctly
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+
+    // Enable WebAssembly and top-level await
     config.experiments = {
-      ...(config.experiments ?? {}),
       asyncWebAssembly: true,
+     //topLevelAwait: true,
+     // syncWebAssembly: true,
     };
+
     return config;
   },
   output: "standalone",
