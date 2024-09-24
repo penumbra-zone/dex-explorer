e8b21cea3f7989420c8fb36ebe02fd2de9ad9ebe
Lucas Meier <lucas@cronokirby.com>
2024-09-24 13:54:18 -0700

    Use generic error handler in api/blocks

--- src/backend/handlers.ts
+++ src/backend/handlers.ts
@@ -1,18 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
-type Handler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
+type Handler<T> = (req: NextApiRequest, res: NextApiResponse<T>) => Promise<void>;
 
 /**
  * Decorate a request handler to catch and return the right status on any error.
+ *
+ * This will map:
+ *  - parsing errors (using zod) to 400 status errors,
+ *  - anything else to 500 status errors.
  */
-export function withCaughtErrors(context: string, f: Handler): Handler {
+export function withCaughtErrors<T>(
+  context: string,
+  f: Handler<T>,
+): Handler<T | { error: string }> {
   return async (req, res) => {
     try {
       await f(req, res);
-    } catch (error) {
-      const message = `Internal error in ${context}: ${String(error)}`;
-      console.error(message);
-      res.status(500).json({ error: message });
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        res.status(400).json({ error: e.toString() });
+      } else {
+        res.status(500).json({ error: `internal error [${context}]: ${String(e)}` });
+      }
     }
   };
 }
