95b173d00194a3cea2b47e4da0ff332ec87b46d5
Max Korsunov <maxkors2014@gmail.com>
2024-09-25 16:15:37 +0200

    fix: sync bar

--- src/components/header/sync-bar.tsx
+++ src/components/header/sync-bar.tsx
@@ -5,10 +5,10 @@ export const SyncBar = () => {
   return (
     <div className='fixed left-0 top-0 h-1 w-full'>
       {!penumbraStatus.value ? (
-        <Progress value={0} loading />
+        <Progress value={0} loading error={Boolean(penumbraStatusError.value)} />
       ) : (
         <Progress
-          value={penumbraStatus.value.syncPercent ?? 0}
+          value={penumbraStatus.value.syncPercent}
           loading={penumbraStatus.value.updating}
           error={Boolean(penumbraStatusError.value)}
         />
