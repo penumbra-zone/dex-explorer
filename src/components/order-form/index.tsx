2facc3658b4430b0e0d965e3c076c8d064262b72
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-10-25 18:44:43 +0400

    Add slider

--- src/components/order-form/index.tsx
+++ src/components/order-form/index.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { Button } from '@penumbra-zone/ui/Button';
+import { Slider } from '@penumbra-zone/ui/Slider';
+import { Text } from '@penumbra-zone/ui/Text';
+// import { theme } from '@penumbra-zone/ui/src/PenumbraUIProvider/theme';
 import { OrderInput } from './order-input';
 import SegmentedControl from './segmented-control';
 
 export function OrderForm() {
   const [buyAmount, setBuyAmount] = useState(300);
   const [sellAmount, setSellAmount] = useState(400);
+  const [sliderValue, setSliderValue] = useState(500);
 
   return (
     <div>
@@ -21,6 +25,22 @@ export function OrderForm() {
         denominator='UM'
       />
       <OrderInput label='Pay with' value={sellAmount} onChange={setSellAmount} denominator='USDC' />
+      <Slider
+        min={0}
+        max={1000}
+        step={100}
+        defaultValue={sliderValue}
+        showValue={false}
+        onChange={setSliderValue}
+        focusedOutlineColor='#BA4D14'
+        showTrackGaps={true}
+        trackGapBackground='#FAFAFA'
+        showFill={true}
+      />
+      <div className='flex flex-row items-center justify-between'>
+        <Text small>Available Balance</Text>
+        <Text small>{sliderValue}</Text>
+      </div>
       <Button>Submit</Button>
     </div>
   );
