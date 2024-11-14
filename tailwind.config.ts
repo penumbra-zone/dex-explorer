e3611d547fcdd94a5399dfc3663fea6b156870ea
Max Korsunov <maxkors2014@gmail.com>
2024-11-14 22:10:07 +0400

    fix: add shimmering animation to skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,17 @@ export default withPenumbra({
         shimmer:
           'linear-gradient(90deg, rgba(250, 250, 250, 0.05) 0%, rgba(250, 250, 250, 0.10) 100%)',
       },
+      keyframes: {
+        ...tailwindConfig.theme.extend.keyframes,
+        shimmer: {
+          '0%': { left: '-50%' },
+          '100%': { left: '150%' },
+        },
+      },
+      animation: {
+        ...tailwindConfig.theme.extend.animation,
+        shimmer: 'shimmer 2s infinite',
+      },
     },
   },
   plugins: [],
