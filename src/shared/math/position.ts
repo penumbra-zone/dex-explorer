1100828042aa211c441bf46cc43b48405ed421cf
Lucas Meier <lucas@cronokirby.com>
2024-12-09 17:29:33 -0800

    Integrate position logic into range liquidity form

--- src/shared/math/position.ts
+++ src/shared/math/position.ts
@@ -12,7 +12,7 @@ import BigNumber from 'bignumber.js';
 //
 // For example, if this is set to 6, we should be able to represent PRICE * 10**6 as a number.
 // In the year 202X, when 1 BTC = 1 million USD, then this is still only 1e12 < 2^50.
-const PRECISION_DECIMALS = 6;
+const PRECISION_DECIMALS = 12;
 
 export interface Asset {
   id: AssetId;
@@ -23,7 +23,7 @@ export interface PositionPlan {
   baseAsset: Asset;
   quoteAsset: Asset;
   price: number;
-  fee_bps: number;
+  feeBps: number;
   baseReserves: number;
   quoteReserves: number;
 }
@@ -51,7 +51,7 @@ export const planToPosition = (plan: PositionPlan): Position => {
   return new Position({
     phi: {
       component: {
-        fee: plan.fee_bps,
+        fee: plan.feeBps,
         p: pnum(p).toAmount(),
         q: pnum(q).toAmount(),
       },
@@ -64,5 +64,50 @@ export const planToPosition = (plan: PositionPlan): Position => {
     state: new PositionState({ state: PositionState_PositionStateEnum.OPENED }),
     reserves: { r1, r2 },
     closeOnFill: false,
+  });
+};
+
+interface RangeLiquidityPlan {
+  baseAsset: Asset;
+  quoteAsset: Asset;
+  targetLiquidity: number;
+  upperPrice: number;
+  lowerPrice: number;
+  marketPrice: number;
+  feeBps: number;
+  positions: number;
+}
+
+export const rangeLiquidityPositions = (plan: RangeLiquidityPlan): Position[] => {
+  // The step width is positions-1 because it's between the endpoints
+  // |---|---|---|---|
+  // 0   1   2   3   4
+  //   0   1   2   3
+  const stepWidth = (plan.upperPrice - plan.lowerPrice) / plan.positions;
+  return Array.from({ length: plan.positions }, (_, i) => {
+    const price = plan.lowerPrice + i * stepWidth;
+
+    let baseReserves: number;
+    let quoteReserves: number;
+    if (price < plan.marketPrice) {
+      // If the price is < market price, then people *paying* that price are getting a good deal,
+      // and receiving the base asset in exchange, so we don't want to offer them any of that.
+      baseReserves = 0;
+      quoteReserves = plan.targetLiquidity / plan.positions;
+    } else {
+      // Conversely, when price > market price, then the people that are selling the base asset,
+      // receiving the quote asset in exchange are getting a good deal, so we don't want to offer that.
+      baseReserves = plan.targetLiquidity / plan.positions / price;
+      quoteReserves = 0;
+    }
+
+    return planToPosition({
+      baseAsset: plan.baseAsset,
+      quoteAsset: plan.quoteAsset,
+      feeBps: plan.feeBps,
+      price,
+      baseReserves,
+      quoteReserves,
+    });
   });
 };
