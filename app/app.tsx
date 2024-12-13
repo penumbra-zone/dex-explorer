c19a32240f9c36967d506dfd9aa7e09cf4b60ac5
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-12-13 21:02:17 +0400

    Fix hydration issue caused by connectionStore.connected

--- app/app.tsx
+++ app/app.tsx
@@ -1,16 +1,24 @@
 'use client';
 
-import { ReactNode } from 'react';
-import { enableStaticRendering } from 'mobx-react-lite';
+import { ReactNode, useEffect } from 'react';
+import { enableStaticRendering, observer } from 'mobx-react-lite';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from '@penumbra-zone/ui/Toast';
 import { TooltipProvider } from '@penumbra-zone/ui/Tooltip';
 import { Header, SyncBar } from '@/widgets/header';
 import { queryClient } from '@/shared/const/queryClient';
+import { connectionStore } from '@/shared/model/connection';
 
 // Used so that observer() won't subscribe to any observables used in an SSR environment
 // and no garbage collection problems are introduced.
 enableStaticRendering(typeof window === 'undefined');
+
+const SetupConnection = observer(() => {
+  useEffect(() => {
+    connectionStore.setup();
+  }, []);
+  return null;
+});
 
 export const App = ({ children }: { children: ReactNode }) => {
   return (
@@ -18,6 +26,7 @@ export const App = ({ children }: { children: ReactNode }) => {
       <TooltipProvider>
         <main className='relative z-0'>
           <SyncBar />
+          <SetupConnection />
           <Header />
           {children}
         </main>
