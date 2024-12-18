c5fe6b26cf53064f53c5ec5cc1961ebdefb520c6
Max Korsunov <maxkors2014@gmail.com>
2024-12-18 15:51:03 +0400

    feat: use balances in the search results

--- src/pages/trade/ui/pair-selector/search-results.tsx
+++ src/pages/trade/ui/pair-selector/search-results.tsx
@@ -1,11 +1,25 @@
 import { observer } from 'mobx-react-lite';
 import { Metadata } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import {
+  getAddressIndex,
+  getBalanceView,
+  getMetadataFromBalancesResponse,
+} from '@penumbra-zone/getters/balances-response';
 import { Text } from '@penumbra-zone/ui/Text';
 import { Dialog } from '@penumbra-zone/ui/Dialog';
 import { AssetIcon } from '@penumbra-zone/ui/AssetIcon';
+import {
+  groupAndSortBalances,
+  AssetSelectorValue,
+  isBalancesResponse,
+} from '@penumbra-zone/ui/AssetSelector';
 import { Button } from '@penumbra-zone/ui/Button';
 import { useAssets } from '@/shared/api/assets';
 import { recentPairsStore } from './store';
+import { useBalances } from '@/shared/api/balances';
+import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
+import { connectionStore } from '@/shared/model/connection';
+import { ValueViewComponent } from '@penumbra-zone/ui/ValueView';
 
 export interface SearchResultsProps {
   onSelect: (asset: Metadata) => void;
@@ -13,20 +27,50 @@ export interface SearchResultsProps {
   search?: string;
 }
 
-const useFilteredAssets = (assets: Metadata[], search: string) => {
-  const sortedAssets = assets.sort((a, b) => Number(b.priorityScore - a.priorityScore));
-  return sortedAssets.filter(asset => {
-    return (
-      asset.symbol.toLowerCase().includes(search.toLowerCase()) ||
-      asset.description.toLowerCase().includes(search.toLowerCase())
-    );
+const filterAsset = (asset: Metadata, search: string): boolean => {
+  return (
+    asset.symbol.toLowerCase().includes(search.toLowerCase()) ||
+    asset.description.toLowerCase().includes(search.toLowerCase())
+  );
+};
+
+const useFilteredAssets = (options: AssetSelectorValue[], search: string) => {
+  return options.filter(option => {
+    if (isBalancesResponse(option)) {
+      const metadata = getMetadataFromBalancesResponse(option);
+      return filterAsset(metadata, search);
+    }
+    return filterAsset(option, search);
   });
+};
+
+const mergeOptions = (
+  assets: Metadata[],
+  balances: BalancesResponse[],
+  account: number,
+): AssetSelectorValue[] => {
+  const grouped = groupAndSortBalances(balances);
+  const balancesPerAccount = grouped.find(group => group[0] === account.toString())?.[1] ?? [];
+  const filteredAssets = assets
+    .filter(
+      asset =>
+        !balancesPerAccount.some(
+          balance => getMetadataFromBalancesResponse(balance).symbol === asset.symbol,
+        ),
+    )
+    .sort((a, b) => Number(b.priorityScore) - Number(a.priorityScore));
+  return [...balancesPerAccount, ...filteredAssets];
 };
 
 export const SearchResults = observer(({ onSelect, onClear, search }: SearchResultsProps) => {
   const { recent, add } = recentPairsStore;
+  const { subaccount } = connectionStore;
+
   const { data: assets } = useAssets();
-  const filtered = useFilteredAssets(assets ?? [], search ?? '');
+  const { data: balances } = useBalances();
+
+  const merged = mergeOptions(assets ?? [], balances ?? [], subaccount);
+  const filtered = useFilteredAssets(merged, search ?? '');
 
   const onClick = (asset: Metadata) => {
     add(asset);
@@ -71,28 +115,51 @@ export const SearchResults = observer(({ onSelect, onClear, search }: SearchResu
         <Text small>Search results</Text>
         <Dialog.RadioGroup>
           <div className='flex flex-col gap-1'>
-            {filtered.map(asset => (
-              <Dialog.RadioItem
-                key={asset.symbol}
-                value={asset.symbol}
-                startAdornment={<AssetIcon metadata={asset} size='lg' />}
-                title={
-                  <div className={asset.name ? '' : 'h-10 flex items-center'}>
-                    <Text color='text.primary'>{asset.symbol}</Text>
-                  </div>
-                }
-                description={
-                  asset.name && (
-                    <div className='-mt-2'>
-                      <Text detail color='text.secondary'>
-                        {asset.name}
-                      </Text>
+            {filtered.map(option => {
+              const asset = isBalancesResponse(option)
+                ? getMetadataFromBalancesResponse(option)
+                : option;
+              const balance = isBalancesResponse(option)
+                ? {
+                    addressIndexAccount: getAddressIndex.optional(option)?.account,
+                    valueView: getBalanceView.optional(option),
+                  }
+                : undefined;
+
+              return (
+                <Dialog.RadioItem
+                  key={asset.symbol}
+                  value={asset.symbol}
+                  startAdornment={<AssetIcon metadata={asset} size='lg' />}
+                  endAdornment={
+                    balance && (
+                      <div className='[&_img]:hidden'>
+                        <ValueViewComponent
+                          showSymbol={false}
+                          context='table'
+                          valueView={balance.valueView}
+                        />
+                      </div>
+                    )
+                  }
+                  title={
+                    <div className={asset.name ? '' : 'h-10 flex items-center'}>
+                      <Text color='text.primary'>{asset.symbol}</Text>
                     </div>
-                  )
-                }
-                onSelect={() => onClick(asset)}
-              />
-            ))}
+                  }
+                  description={
+                    asset.name && (
+                      <div className='-mt-2'>
+                        <Text detail color='text.secondary'>
+                          {asset.name}
+                        </Text>
+                      </div>
+                    )
+                  }
+                  onSelect={() => onClick(asset)}
+                />
+              );
+            })}
           </div>
         </Dialog.RadioGroup>
       </div>
