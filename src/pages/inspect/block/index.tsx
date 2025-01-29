a0b6456d2bc127652062ba7d63d23e091679e28f
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-01-29 17:07:11 +0400

    Fetch transactions by height

--- src/pages/inspect/block/index.tsx
+++ src/pages/inspect/block/index.tsx
@@ -1,3 +1,29 @@
+'use client';
+
+import { penumbra } from '@/shared/const/penumbra';
+import { ViewService } from '@penumbra-zone/protobuf';
+import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
+
 export function InspectBlock() {
+  const params = useParams<{ height: string }>();
+  const blockheight = params.height;
+  console.log('TCL: InspectBlock -> blockheight', blockheight);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const transactions = await Array.fromAsync(
+        penumbra.service(ViewService).transactionInfo({
+          startHeight: BigInt(blockheight - 999),
+          endHeight: BigInt(blockheight),
+        }),
+      );
+
+      return transactions;
+    };
+
+    fetchTransactions().then(console.log);
+  }, [blockheight]);
+
   return <div>InspectBlock</div>;
 }
