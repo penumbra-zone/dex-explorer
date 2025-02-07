986b6c3aceec0b1fbb218288a63f12b97e954d6c
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-02-07 16:56:00 +0400

    Handle all transaction actionView cases

--- src/pages/inspect/tx/api/transaction.ts
+++ src/pages/inspect/tx/api/transaction.ts
@@ -33,42 +33,10 @@ import {
   SwapView,
   SwapView_Opaque,
 } from '@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb';
-
-const actionValueViewByCase = {
-  spend: SpendView,
-  output: OutputView,
-  swap: SwapView,
-  swapClaim: SwapClaimView,
-  // ics20Withdrawal: Ics20WithdrawalView,
-  // ics20Deposit: Ics20DepositView,
-  // delegate: DelegateView,
-  // undelegate: UndelegateView,
-  // undelegateClaim: UndelegateClaimView,
-  // actionDutchAuctionSchedule: ActionDutchAuctionScheduleView,
-  // actionDutchAuctionWithdraw: ActionDutchAuctionWithdrawView,
-  // actionDutchAuctionEnd: ActionDutchAuctionEndView,
-  // delegatorVote: DelegatorVoteView,
-  // validatorVote: ValidatorVoteView,
-  positionOpen: PositionOpen,
-  positionClose: PositionClose,
-  positionWithdraw: PositionWithdraw,
-  // validatorDefinition: ValidatorDefinitionView,
-  // ibcRelayAction: IbcRelayActionView,
-  // proposalSubmit: ProposalSubmitView,
-  // proposalWithdraw: ProposalWithdrawView,
-  // proposalDepositClaim: ProposalDepositClaimView,
-  // communityPoolSpend: CommunityPoolSpendView,
-  // communityPoolOutput: CommunityPoolOutputView,
-  // communityPoolDeposit: CommunityPoolDepositView,
-};
-
-const actionValueViewOpaqueByCase = {
-  spend: SpendView_Opaque,
-  output: OutputView_Opaque,
-  swap: SwapView_Opaque,
-  swapClaim: SwapClaimView_Opaque,
-  delegatorVote: DelegatorVoteView_Opaque,
-};
+import {
+  ActionDutchAuctionScheduleView,
+  ActionDutchAuctionWithdrawView,
+} from '@penumbra-zone/protobuf/penumbra/core/component/auction/v1/auction_pb';
 
 export const useTransactionInfo = (txHash: string, connected: boolean) => {
   return useQuery({
@@ -107,39 +75,242 @@ export const useTransactionInfo = (txHash: string, connected: boolean) => {
           anchor: transaction.anchor,
           bindingSig: transaction.bindingSig,
           bodyView: new TransactionBodyView({
-            actionViews: transaction.body?.actions
-              .map(action => {
-                const { case: actionCase, value } = action.action;
-
-                if (value.proof) {
-                  const ActionValueView = actionValueViewByCase[actionCase];
-                  const ActionValueView_Opaque = actionValueViewOpaqueByCase[actionCase];
-
-                  if (!ActionValueView) {
-                    return null;
-                  }
-
+            actionViews: transaction.body?.actions.map(action => {
+              const { case: actionCase, value } = action.action;
+              switch (actionCase) {
+                case 'spend':
                   return new ActionView({
                     actionView: {
-                      case: actionCase,
-                      value: new ActionValueView({
+                      case: 'spend',
+                      value: new SpendView({
                         spendView: {
                           case: 'opaque',
-                          value: new ActionValueView_Opaque({}),
+                          value: new SpendView_Opaque({}),
                         },
                       }),
                     },
                   });
-                } else {
+
+                case 'output':
+                  return new ActionView({
+                    actionView: {
+                      case: 'output',
+                      value: new OutputView({
+                        outputView: {
+                          case: 'opaque',
+                          value: new OutputView_Opaque({}),
+                        },
+                      }),
+                    },
+                  });
+
+                case 'swap':
+                  return new ActionView({
+                    actionView: {
+                      case: 'swap',
+                      value: new SwapView({
+                        swapView: {
+                          case: 'opaque',
+                          value: new SwapView_Opaque({}),
+                        },
+                      }),
+                    },
+                  });
+
+                case 'swapClaim':
+                  return new ActionView({
+                    actionView: {
+                      case: 'swapClaim',
+                      value: new SwapClaimView({
+                        swapClaimView: {
+                          case: 'opaque',
+                          value: new SwapClaimView_Opaque({}),
+                        },
+                      }),
+                    },
+                  });
+
+                case 'delegatorVote':
                   return new ActionView({
                     actionView: {
                       case: actionCase,
+                      value: new DelegatorVoteView({
+                        delegatorVote: {
+                          case: 'opaque',
+                          value: new DelegatorVoteView_Opaque({}),
+                        },
+                      }),
+                    },
+                  });
+
+                case 'validatorDefinition':
+                  return new ActionView({
+                    actionView: {
+                      case: 'validatorDefinition',
                       value,
                     },
                   });
-                }
-              })
-              .filter(Boolean),
+
+                case 'ibcRelayAction':
+                  return new ActionView({
+                    actionView: {
+                      case: 'ibcRelayAction',
+                      value,
+                    },
+                  });
+
+                case 'proposalSubmit':
+                  return new ActionView({
+                    actionView: {
+                      case: 'proposalSubmit',
+                      value,
+                    },
+                  });
+
+                case 'proposalWithdraw':
+                  return new ActionView({
+                    actionView: {
+                      case: 'proposalWithdraw',
+                      value,
+                    },
+                  });
+
+                case 'proposalDepositClaim':
+                  return new ActionView({
+                    actionView: {
+                      case: 'proposalDepositClaim',
+                      value,
+                    },
+                  });
+
+                case 'validatorVote':
+                  return new ActionView({
+                    actionView: {
+                      case: 'validatorVote',
+                      value,
+                    },
+                  });
+
+                case 'positionOpen':
+                  return new ActionView({
+                    actionView: {
+                      case: 'positionOpen',
+                      value,
+                    },
+                  });
+
+                case 'positionClose':
+                  return new ActionView({
+                    actionView: {
+                      case: 'positionClose',
+                      value,
+                    },
+                  });
+
+                case 'positionWithdraw':
+                  return new ActionView({
+                    actionView: {
+                      case: 'positionWithdraw',
+                      value,
+                    },
+                  });
+
+                case 'positionRewardClaim':
+                  return new ActionView({
+                    actionView: {
+                      case: 'positionRewardClaim',
+                      value,
+                    },
+                  });
+
+                case 'delegate':
+                  return new ActionView({
+                    actionView: {
+                      case: 'delegate',
+                      value,
+                    },
+                  });
+
+                case 'undelegate':
+                  return new ActionView({
+                    actionView: {
+                      case: 'undelegate',
+                      value,
+                    },
+                  });
+
+                case 'communityPoolSpend':
+                  return new ActionView({
+                    actionView: {
+                      case: 'communityPoolSpend',
+                      value,
+                    },
+                  });
+
+                case 'communityPoolOutput':
+                  return new ActionView({
+                    actionView: {
+                      case: 'communityPoolOutput',
+                      value,
+                    },
+                  });
+
+                case 'communityPoolDeposit':
+                  return new ActionView({
+                    actionView: {
+                      case: 'communityPoolDeposit',
+                      value,
+                    },
+                  });
+
+                case 'actionDutchAuctionSchedule':
+                  return new ActionView({
+                    actionView: {
+                      case: 'actionDutchAuctionSchedule',
+                      value: new ActionDutchAuctionScheduleView({
+                        action: value,
+                      }),
+                    },
+                  });
+
+                case 'actionDutchAuctionWithdraw':
+                  return new ActionView({
+                    actionView: {
+                      case: 'actionDutchAuctionWithdraw',
+                      value: new ActionDutchAuctionWithdrawView({
+                        action: value,
+                      }),
+                    },
+                  });
+
+                case 'actionDutchAuctionEnd':
+                  return new ActionView({
+                    actionView: {
+                      case: 'actionDutchAuctionEnd',
+                      value,
+                    },
+                  });
+
+                case 'undelegateClaim':
+                  return new ActionView({
+                    actionView: {
+                      case: 'undelegateClaim',
+                      value,
+                    },
+                  });
+
+                case 'ics20Withdrawal':
+                  return new ActionView({
+                    actionView: {
+                      case: 'ics20Withdrawal',
+                      value,
+                    },
+                  });
+
+                default:
+                  return null;
+              }
+            }),
             transactionParameters: transaction.body?.transactionParameters,
             detectionData: transaction.body?.detectionData,
             memoView: new MemoView({
