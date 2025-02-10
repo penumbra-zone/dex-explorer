5887cc592e7ddc82bfebd2878b24a83215c091bb
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-02-10 17:41:46 +0400

    Readd twMerge

--- src/pages/inspect/tx/ui/tx-view-component/utils/cn.tsx
+++ src/pages/inspect/tx/ui/tx-view-component/utils/cn.tsx
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]): string {
-  return clsx(inputs);
+  return twMerge(clsx(inputs));
 }
