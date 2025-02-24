339070a44f46d85af12e6ced4b3b9874118537d0
Atris <vacekj@outlook.com>
2025-02-24 16:42:51 +0100

    Quick commit 2025-02-24 16:42:51

---
+++ src/features/cosmos/types.ts
@@ -0,0 +1,23 @@
+import type { Asset, Chain } from '@chain-registry/types';
+
+export interface Balance {
+    denom: string;
+    amount: string;
+    chain: string;
+    symbol?: string;
+    displayAmount?: string;
+}
+
+export interface ChainConfig {
+    chainId: string;
+    restEndpoint: string;
+    chain: Chain & { bech32_prefix: string };
+    assets: Asset[];
+}
+
+export interface BankBalancesResponse {
+    balances: {
+        denom: string;
+        amount: string;
+    }[];
+} 
