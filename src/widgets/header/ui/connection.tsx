fd24bdbeaa0dc7ca5722fcab5d4a59da2f51c7e4
Atris <vacekj@outlook.com>
2025-02-24 16:09:35 +0100

    Quick commit 2025-02-24 16:09:35

--- src/widgets/header/ui/connection.tsx
+++ src/widgets/header/ui/connection.tsx
@@ -2,11 +2,15 @@ import { observer } from 'mobx-react-lite';
 import { connectionStore } from '@/shared/model/connection';
 import { ConnectButton } from '@/features/connect/connect-button';
 import { SubaccountSelector } from '@/widgets/header/ui/subaccount-selector';
+import { CosmosBalances } from '@/features/cosmos/keplr-balances';
 
 export const Connection = observer(() => {
-  if (!connectionStore.connected) {
-    return <ConnectButton />;
-  }
-
-  return <SubaccountSelector />;
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center gap-4'>
+        {!connectionStore.connected ? <ConnectButton /> : <SubaccountSelector />}
+        <CosmosBalances />
+      </div>
+    </div>
+  );
 });
