b45a8d5ad8ad21f4b6643e72fe5a48225c300f2e
Atris <vacekj@outlook.com>
2025-03-22 00:33:34 +0100

    feat: integrate onboarding

--- src/pages/portfolio/ui/onboarding.tsx
+++ src/pages/portfolio/ui/onboarding.tsx
@@ -11,7 +11,7 @@ import { PenumbraClient } from '@penumbra-zone/client';
 import { connectionStore } from '@/shared/model/connection';
 import { useProviderManifests } from '@/shared/api/providerManifests';
 import { CosmosConnectButton } from '@/features/cosmos/cosmos-connect-button';
-import { useUnifiedAssets } from '../hooks/use-unified-assets';
+import { useUnifiedAssets } from '../api/use-unified-assets';
 
 const OnboardingCard = ({
   title,
