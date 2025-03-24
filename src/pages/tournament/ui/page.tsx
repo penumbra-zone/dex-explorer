fe8b190345a009b2f3764a6b76c0636b580d2028
Max Korsunov <maxkors2014@gmail.com>
2025-03-24 11:29:41 +0900

    fix: #2117: after merge

--- src/pages/tournament/ui/page.tsx
+++ src/pages/tournament/ui/page.tsx
@@ -2,11 +2,13 @@
 
 import { PenumbraWaves } from '@/pages/explore/ui/waves';
 import { MyRewards } from './my-rewards';
+import { LandingCard } from './landing-card';
 
 export const TournamentPage = () => {
   return (
     <section className='flex flex-col gap-6 p-4 max-w-[1062px] mx-auto'>
       <PenumbraWaves />
+      <LandingCard />
       <MyRewards />
     </section>
   );
